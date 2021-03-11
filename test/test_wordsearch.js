8b8ba95f5332cbd9ead9f362f58a69d6ffb3cd4b
Sarah <sarah.pflug@shopify.com>
2021-03-11 04:49:51 +0000

    pair programming session with Summer

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,35 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return empty string if no word is given", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.deepEqual('', result);
+  });
+
+  it("should return true if the word is written vertically ", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['P', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Z', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['Z', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['A', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APIZZAUOE')
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(letter => letter.join(''));
 
-module.exports = wordSearch
+  const transpose = function(matrix) {
+    const result = [];
+    for (let x = 0; x < matrix[0].length; x++) {
+      const buffer = [];
+      for (let y = 0; y < matrix.length; y++) {
+        buffer.push(matrix[y][x]);
+      }
+      result.push(buffer);
+    }
+    return result;
+  };
+
+  const verticalJoin = transpose(letters).map(letter => letter.join(''));
+  console.log(verticalJoin);
+  if (word === "") {
+    return "";
+  }
+
+  for (let item of horizontalJoin) {
+    if (item.includes(word)) {
+      return true;
+    }
+  }
+
+  for (let item of verticalJoin) {
+    if (item.includes(word)) {
+      return true;
+    }
+  } return false;
+
+};
+module.exports = wordSearch;
+
+// wordSearch(['l'], 'hello');
